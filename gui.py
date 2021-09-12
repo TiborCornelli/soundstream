04a6f0c9a2e7fb1d7b6f3cf3d93b36fcca1de0b3
TiborCornelli <tibor.cornelli@gmail.com>
2021-09-12 22:51:45 +0200

    Added a warning button

--- gui.py
+++ gui.py
@@ -28,6 +28,9 @@ class App(tk.Tk):
         # button to submit url
         self.button = Button(self, text="Download", command=self.call_downloader)
         self.button.grid(row=1, column=2)
+
+        # Warning label
+        self.warning = Label(self, fg="red", text="Location or URL are invalid")
     
     def set_path(self, path: str) -> None:
         self.PATH: str = path
@@ -38,9 +41,14 @@ class App(tk.Tk):
         self.path_input["text"] = folder_selected # Button shows the currently chosen path
 
     def call_downloader(self) -> None:
-        url = self.url_input.get()
-        path = self.PATH
-        self.url_input.delete(0,"end") # url input is cleared after clicking the button
-        download(url, path) # download() is imported from the downloader.py
+        try:
+            url = self.url_input.get()
+            path = self.PATH
+            self.url_input.delete(0,"end") # url input is cleared after clicking the button
+            download(url, path) # download() is imported from the downloader.py
+            self.warning.grid_forget() # Hides warning if no problems occur
+        except:
+            self.warning.grid(row=2, column=1)
+            
 
-app = App().mainloop() 
+app = App().mainloop()
